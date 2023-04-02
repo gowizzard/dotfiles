7f209458eb49788378f8f0c513a729f627cebc9e
Jonas Kwiedor <mail@gowizzard.de>
2023-04-02 12:21:15 +0200

    chore: Update dotfiles script.

--- .scripts/dotfiles.lua
+++ .scripts/dotfiles.lua
@@ -1,30 +1,57 @@
--- Save the dotfiles to the dotfiles repo
-local home_directory = os.getenv("HOME")
-local projects_directory = os.getenv("PROJECTS_DIRECTORY")
+-- Save and restore dotfiles from a git repository or to the git repository
+local directories = {
+    home = os.getenv("HOME"),
+    projects = os.getenv("PROJECTS_DIRECTORY")
+}
 
 local files = {
     ".gitconfig",
     ".tmux.conf",
     ".zshrc",
     ".config/nvim/init.lua",
-    ".config/nvim/lua/*",
-    ".scripts/*"
+    ".config/nvim/lua/default.lua",
+    ".scripts/dotfiles.lua"
 }
+
+local function copy_file(source, destination) 
+    
+    local source_file = io.open(source, "r")
+    if source_file == nil then
+        print("file " .. source .. " does not exist")
+        return
+    end
+
+    local destination_file = io.open(destination, "w")
+    if destination_file == nil then
+        print("file " .. destination .. " does not exist")
+        return
+    end
+
+    destination_file:write(source_file:read("*all"))
+
+    source_file:close()
+    destination_file:close()
+
+end
 
 if arg[1] == "backup" then
 
     for _, value in pairs(files) do
-        os.execute("cp -r " .. home_directory .. "/" .. value .. " " .. projects_directory .. "/dotfiles/" .. value)
+
+        copy_file(directories.home .. "/" .. value, directories.projects .. "/dotfiles/" .. value)
+
     end
 
-    os.execute("cd " .. projects_directory .. "/dotfiles; git aac \"ci: The data was added automatically.\"")
+    os.execute("cd " .. directories.projects .. "/dotfiles; git aac \"ci: The data was added automatically.\"")
 
     print("dotfiles backed up successfully")
 
 elseif arg[1] == "restore" then
     
     for _, value in pairs(files) do
-        os.execute("cp -r " .. projects_directory .. "/dotfiles/" .. value .. " " .. home_directory .. "/" .. value)
+
+        copy_file(directories.projects .. "/dotfiles/" .. value, directories.home .. "/" .. value)
+
     end
 
     print("dotfiles restored successfully")
