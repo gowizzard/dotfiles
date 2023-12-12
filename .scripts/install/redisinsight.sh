9c95f2362dd3a758b3603ebc1fb87652aa984013
Jonas Kwiedor <mail@gowizzard.de>
2023-12-12 20:57:43 +0100

    chore: Update file mode & function.

--- .scripts/install/redisinsight.sh
+++ .scripts/install/redisinsight.sh
@@ -7,18 +7,16 @@ if [ -d "/opt/redisinsight" ]; then
     exit 1
 fi
 
-sudo mkdir -p /opt/redinsight
 
 wget -O /tmp/redisinsight.tar.gz https://download.redisinsight.redis.com/latest/redisstack/RedisInsight-app-linux.x86_64.tar.gz
-
 if [ ! -f "/tmp/redisinsight.tar.gz" ]; then
     echo "Error while downloading RedisInsight."
     exit 1
 fi
 
-sudo tar -xzf /tmp/redisinsight.tar.gz -C /opt/redisinsight
-
-rm /tmp/redisinsight.tar.gz
+sudo mkdir -p /opt/redisinsight
+sudo tar -xzf /tmp/redisinsight.tar.gz -C /opt/redisinsight --strip-components=1
+sudo rm /tmp/redisinsight.tar.gz
 
 if [ ! -L "/usr/local/bin/redisinsight" ]; then
     sudo ln -s /opt/redisinsight/redisinsight /usr/local/bin/redisinsight
