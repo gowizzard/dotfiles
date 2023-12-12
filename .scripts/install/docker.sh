621b2857549f30dd46e23c30295cbe4d27b7e8ac
Jonas Kwiedor <mail@gowizzard.de>
2023-12-12 21:06:39 +0100

    chore: Add proof for docker service is running.

--- .scripts/install/docker.sh
+++ .scripts/install/docker.sh
@@ -2,6 +2,11 @@
 
 # Install docker via package manager and enable service / add user to docker group.
 
+if sudo systemctl is-active --quiet docker.service; then
+    echo "Docker is already installed."
+    exit 1
+fi
+
 sudo pacman -S --noconfirm docker docker-compose
 
 sudo systemctl enable docker.service
