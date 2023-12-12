a5f1ec367582c2d477e840700172abba0fa96924
Jonas Kwiedor <mail@gowizzard.de>
2023-12-12 11:12:42 +0100

    chore: Add installation script for redisinsight.

---
+++ .scripts/install/redisinsight.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install redisinsight an gui for redis databases.
+
+if [ -d "/opt/redisinsight" ]; then
+    echo "RedisInsight is already installed."
+    exit 1
+fi
+
+sudo mkdir -p /opt/redinsight
+
+wget -O /tmp/redisinsight.tar.gz https://download.redisinsight.redis.com/latest/redisstack/RedisInsight-app-linux.x86_64.tar.gz
+
+if [ ! -f "/tmp/redisinsight.tar.gz" ]; then
+    echo "Error while downloading RedisInsight."
+    exit 1
+fi
+
+sudo tar -xzf /tmp/redisinsight.tar.gz -C /opt/redisinsight
+
+rm /tmp/redisinsight.tar.gz
+
+if [ ! -L "/usr/local/bin/redisinsight" ]; then
+    sudo ln -s /opt/redisinsight/redisinsight /usr/local/bin/redisinsight
+fi
+
+echo "RedisInsight installation completed."
