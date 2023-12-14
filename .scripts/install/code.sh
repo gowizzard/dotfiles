c9be7c680b55fd83bd8cce9f8cc38781fb89cde2
Jonas Kwiedor <mail@gowizzard.de>
2023-12-14 12:39:22 +0100

    chore: Add installation script for visual studio code.

---
+++ .scripts/install/code.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install GitKraken latest version for linux x64 systems.
+
+if [ -d "/opt/code" ]; then
+    sudo rm -rf /opt/code
+fi
+
+wget -O /tmp/code.tar.gz https://code.visualstudio.com/sha/download?build=stable&os=linux-x64
+if [ ! -f "/tmp/code.tar.gz" ]; then
+    echo "Error while downloading Visual Studio Code."
+    exit 1
+fi
+
+sudo mkdir -p /opt/code
+sudo tar -xzf /tmp/code.tar.gz -C /opt/code --strip-components=1
+rm /tmp/code.tar.gz
+
+if [ ! -L "/usr/local/bin/code" ]; then
+    sudo ln -s /opt/code/code /usr/local/bin/code
+fi
+
+echo "Visual Sturio Code installation completed."
