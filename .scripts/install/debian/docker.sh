3b96376ad4415cab67876f28fd27d08b83cec870
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2024-01-03 11:35:22 +0100

    chore: Update debian scripts.

--- .scripts/install/debian/docker.sh
+++ .scripts/install/debian/docker.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+# Add docker repository to apt and install the debian package.
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker is already installed."
+    exit 0
+fi
+
+sudo apt update
+sudo apt install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -10,8 +16,10 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo docker run hello-world
+
+echo "Docker installation completed."
---
+++ .scripts/install/debian/terraform.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Add terraform repository to apt and install the debian package.
+if [ -x "$(command -v terraform)" ]; then
+    echo "Terraform is already installed."
+    exit 0
+fi
+
+sudo apt update
+sudo apt install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+sudo apt install -y terraform
+
+echo "Terraform installation completed."
