86f745b442bfcdff0a3fa251e3168058a6b09f45
Jonas Kwiedor <mail@gowizzard.de>
2025-02-02 19:26:13 +0100

    chore: add init file for hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,20 @@
+-- This is my hammerspoon configuration file. It is a Lua script that is executed by Hammerspoon when it starts up. 
+-- It is located at ~/.hammerspoon/init.lua. This file is a great place to define hotkeys, functions, and other configuration options for Hammerspoon.
+local function reload(files)
+    for _, v in ipairs(files) do
+        if v:match("%.lua$") then
+            hs.reload()
+            return
+        end
+    end
+end
+
+path = hs.configdir .. "/init.lua"
+watcher = hs.pathwatcher.new(path, reload):start()
+hs.notify.new({title="Hammerspoon", informativeText="Config file in ~/.hammerspoon/init.lua reloaded"}):send()
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+    hs.reload()
+end)
+
+hs.hotkey.bind({"cmd", "alt"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
