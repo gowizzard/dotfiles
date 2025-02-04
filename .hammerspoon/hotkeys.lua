1cd7ea58abfdd1131a0e65028bc06bc0da3df9a3
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2025-02-04 15:09:26 +0100

    chore: update hammerspoon configuration

--- .hammerspoon/hotkeys.lua
+++ .hammerspoon/hotkeys.lua
@@ -1,6 +1,6 @@
 -- In this file, I define all my hotkey functions. 
 -- I can then require this file in my init.lua file to keep my configuration file clean and easy to read.
-local mod = {"cmd", "alt"}
+local hyper = {"cmd", "alt", "ctrl", "shift"}
 
-hs.hotkey.bind(mod, "R", function() hs.reload() end)
-hs.hotkey.bind(mod, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+hs.hotkey.bind(hyper, "R", function() hs.reload() end)
+hs.hotkey.bind(hyper, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
