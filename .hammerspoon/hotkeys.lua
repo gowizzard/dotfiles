a5c26f19798cb185613dc254443d7d3d4d82671b
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2025-02-05 11:58:55 +0100

    chore: update hammerspoon config

--- .hammerspoon/hotkeys.lua
+++ .hammerspoon/hotkeys.lua
@@ -5,3 +5,21 @@ local hyper = {"cmd", "ctrl", "option", "shift"}
 hs.hotkey.bind(hyper, "r", function() notifications("reload_config") end)
 hs.hotkey.bind(hyper, "l", function() hs.caffeinate.lockScreen() end)
 hs.hotkey.bind(hyper, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+
+hs.hotkey.bind(hyper, "o", function()
+    apps = {
+        ["a"] = "Arc",
+        ["c"] = "ChatGPT",
+        ["d"] = "Discord",
+        ["f"] = "Finder",
+        ["m"] = "Thunderbird",
+        ["n"] = "Notion",
+        ["p"] = "Spotify",
+        ["s"] = "Slack",
+        ["t"] = "Ghostty",
+    }
+
+    for i, v in pairs(apps) do
+        hs.hotkey.bind(hyper, i, function() hs.application.launchOrFocus(v) end)
+    end
+end)
