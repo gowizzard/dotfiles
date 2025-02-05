03714ddf0ddaa088c280b1af05c59537a60c09fd
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2025-02-05 10:05:25 +0100

    chore: update hammerspoon config

--- .hammerspoon/hotkeys.lua
+++ .hammerspoon/hotkeys.lua
@@ -1,6 +1,10 @@
 -- In this file, I define all my hotkey functions. 
 -- I can then require this file in my init.lua file to keep my configuration file clean and easy to read.
-local hyper = {"cmd", "alt", "ctrl", "shift"}
+local hyper = {"cmd", "ctrl", "option", "shift"}
 
-hs.hotkey.bind(hyper, "R", function() hs.reload() end)
-hs.hotkey.bind(hyper, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+hs.hotkey.bind(hyper, "r", function()
+    reloadConfig()
+end)
+
+hs.hotkey.bind(hyper, "l", function() hs.caffeinate.lockScreen() end)
+hs.hotkey.bind(hyper, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
---
+++ .hammerspoon/utils.lua
@@ -0,0 +1,6 @@
+-- In this file, I define little utility functions that I use in my other files like hotkeys.lua and pathwatchers.lua.
+-- I can then require this file in my other files to keep my configuration files clean and easy to read.
+function reloadConfig() 
+    hs.notify.new({title="Hammerspoon", informativeText="Config files in ~/.hammerspoon reloaded"}):send()
+    hs.reload()
+end
