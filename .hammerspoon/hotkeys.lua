c8f7a86dafbaf23e163b54e4a3f13417efadc9f9
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2025-02-08 12:01:07 +0100

    chore: update lua hammerspoon configuration

--- .hammerspoon/hotkeys.lua
+++ .hammerspoon/hotkeys.lua
@@ -5,13 +5,15 @@ local hyper = {"cmd", "ctrl", "option", "shift"}
 hs.hotkey.bind(hyper, "r", function() notifications("reload_config") end)
 hs.hotkey.bind(hyper, "x", function() hs.toggleConsole() end)
 
-hs.hotkey.bind(hyper, "q", function() hs.caffeinate.shutdownSystem() end)
-hs.hotkey.bind(hyper, "s", function() hs.caffeinate.systemSleep() end)
-
-hs.hotkey.bind(hyper, "f", function() hs.application.launchOrFocus("Finder") end)
-
 hs.hotkey.bind(hyper, "right", function() hs.window.focusedWindow():moveOneScreenEast() end)
 hs.hotkey.bind(hyper, "left", function() hs.window.focusedWindow():moveOneScreenWest() end)
 hs.hotkey.bind(hyper, "return", function() hs.window.focusedWindow():maximize() end)
 hs.hotkey.bind(hyper, "h", function() hs.window.focusedWindow():moveToUnit(hs.layout.left50) end)
 hs.hotkey.bind(hyper, "l", function() hs.window.focusedWindow():moveToUnit(hs.layout.right50) end)
+
+local caffeinate = hs.hotkey.modal.new(hyper, "c")
+
+caffeinate:bind({}, "escape", function() caffeinate:exit() end)
+
+caffeinate:bind({}, "q", function() hs.caffeinate.shutdownSystem() caffeinate:exit() end)
+caffeinate:bind({}, "s", function() hs.caffeinate.systemSleep() caffeinate:exit() end)
