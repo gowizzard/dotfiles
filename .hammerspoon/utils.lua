dfd84145ae68ac3ee6b0b8f513016a3c40079be2
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2025-02-09 17:12:06 +0100

    chore: update hammerspoon configuration

--- .hammerspoon/utils.lua
+++ .hammerspoon/utils.lua
@@ -1,6 +1,14 @@
 -- In this file, I define little utility functions that I use in my other files like hotkeys.lua and pathwatchers.lua.
 -- I can then require this file in my other files to keep my configuration files clean and easy to read.
 function reload()
-    hs.notify.new({autoWithdraw=true, informativeText="Config files in ~/.hammerspoon reloaded", title="Hammerspoon", withdrawAfter=2}):send()
-    hs.reload()
+    local notification = hs.notify.new({
+        informativeText = "Config files in ~/.hammerspoon reloaded",
+        title = "Hammerspoon",
+    })
+    notification:send()
+    
+    hs.timer.doAfter(2, function()
+        notification:withdraw()
+        hs.reload()
+    end)
 end
