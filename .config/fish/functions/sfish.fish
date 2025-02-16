60f03f3acac147e933495ebfc3787a420a7d59c1
Jonas Kwiedor <mail@gowizzard.de>
2025-02-16 08:46:48 +0100

    chore: remove unused fish functions

--- .config/fish/functions/sfish.fish
+++
@@ -1,4 +0,0 @@
-# The function sfish is to source the config.fish file.
-function sfish
-    source ~/.config/fish/config.fish
-end
--- .config/fish/functions/stmux.fish
+++
@@ -1,4 +0,0 @@
-# The function stmux is to source the tmux.conf file.
-function stmux
-    tmux source-file ~/.tmux.conf
-end
