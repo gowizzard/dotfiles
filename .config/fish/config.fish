0ad21df027bf4f7c47e00da90360c8befd78bfbd
Jonas Kwiedor <mail@gowizzard.de>
2025-02-22 16:35:12 +0100

    chore: update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,14 @@
-# Add files to source here. This is the main configuration file for fish shell.
-# Source the functions and completions.
-starship init fish | source
-source $HOME/.config/op/plugins.sh
-
+# This is the main configuration file for fish shell. It is loaded every time a new shell is opened.
 # Add abbreviations here. Abbreviations are shortcuts for commands. For example, 
 # `abbr -a vim nvim` will allow you to use `vim` as a shortcut for `nvim`.
 abbr -a vim nvim
 abbr -a make gmake
 abbr -a cat bat
 abbr -a neofetch fastfetch
+
+# Add files or initialization scripts here to be loaded every time a new shell is opened. 
+source $HOME/.config/op/plugins.sh
+
+starship init fish | source
+atuin init fish | source
+zoxide init fish | source
