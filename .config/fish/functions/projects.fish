f68518be092cc5ea19a7ce7dd40a444e9e15cfd8
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2025-02-28 13:53:29 +0100

    chore: update fish shell

--- .config/fish/functions/projects.fish
+++ .config/fish/functions/projects.fish
@@ -18,7 +18,7 @@ function projects
         set -a formatted_directories $name
     end
     
-    set -l selected_project (printf "%s\n" $formatted_directories | fzf --height 25% --reverse --border --prompt="Select project: ")
+    set -l selected_project (printf "%s\n" $formatted_directories | fzf --height 25% --reverse --border --prompt="Select project: " --query="$argv[1]")
     if test -n "$selected_project"
         cd "$PROJECTS_DIRECTORY/$selected_project"
     end
