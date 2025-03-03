53c51ee08331397df69185510d64472bf4eec5e0
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2025-03-03 10:46:13 +0100

    chore: update fish configuration

---
+++ .config/fish/functions/update_all.fish
@@ -0,0 +1,15 @@
+# The function update_all is used to update Homebrew, Brewfile, pipx packages, and fish completions.
+# First, it updates Homebrew and Brewfile by running brew update, brew upgrade, and brew cleanup.
+# Then, it updates pipx packages by running pipx upgrade-all.
+# Finally, it updates fish completions by running fish_update_completions.
+function update_all
+    brew update && brew upgrade && brew cleanup
+    brew bundle dump --file=$DOTFILES_DIRECTORY/Brewfile --force
+    echo "🍻 Homebrew and Brewfile updated."
+
+    pipx upgrade-all
+    echo "🐍 Pipx packages updated."
+
+    fish_update_completions
+    echo "🐟 Fish completions updated."
+end
