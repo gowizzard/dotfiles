ece40f51dc43c0eb21736f7028168ecf6af95a8c
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2025-04-15 17:43:24 +0200

    chore: remove docker completions file

--- .config/fish/completions/docker.fish
+++
@@ -1,6 +0,0 @@
-# Autocompletion for docker commands in fish shell. The `-f` flag is used to force the completion.
-complete -c docker -n '__fish_seen_subcommand_from logs' -a '(docker ps --format "{{.Names}}")' -f
-complete -c docker -n '__fish_seen_subcommand_from exec' -a '(docker ps --format "{{.Names}}")' -f
-complete -c docker -n '__fish_seen_subcommand_from stop' -a '(docker ps --format "{{.Names}}")' -f
-complete -c docker -n '__fish_seen_subcommand_from restart' -a '(docker ps --format "{{.Names}}")' -f
-complete -c docker -n '__fish_seen_subcommand_from rm' -a '(docker ps --format "{{.Names}}")' -f
